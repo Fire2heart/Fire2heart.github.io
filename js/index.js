1f42c27db9290f696c595a196c91866d656eba0c
Anthony_Grid <52fire2heart52@gmail.com>
2022-07-30 18:43:03 +0300

    v1.1 version for phones, bug with touch screen wtf

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
     $('.care__name').click(function (event) {
         $(this).toggleClass('active').next().slideToggle(300);
     });
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__options').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
